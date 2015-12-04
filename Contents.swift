d086855e4cca14e710c0eda359bd21d5d1a38c39
pako10 <pakoandrade10@gmail.com>
2015-12-03 21:33:48 -0600

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -8,20 +8,48 @@ enum Velocidades:Int{
     case VelocidadBaja = 20
     case VelocidadMedia = 50
     case VelocidadAlta = 120
+    
     init(velocidadInicial:Velocidades){
-        self = .Apagado
+        self = velocidadInicial
     }
 }
 
 class Auto{
-    var velocidad = Velocidades.self
+    var velocidad : Velocidades
     
     init(){
-        
+        self.velocidad = Velocidades.Apagado
     }
     
     func cambioDeVelocidad()->(actual:Int,velocidadEnCadena:String){
+        let actual = self.velocidad
+        var speed : String = ""
         
+        switch self.velocidad{
+        case.Apagado:
+            self.velocidad = .VelocidadBaja
+            speed = "Velocidad Baja"
+
+        case .VelocidadBaja:
+            self.velocidad = .VelocidadMedia
+            speed = "Velocidad Media"
+        case .VelocidadMedia:
+            self.velocidad = .VelocidadAlta
+            speed = "Velocidad Alta"
+        case .VelocidadAlta:
+            self.velocidad = .VelocidadBaja
+            speed = "Velocidad Baja"
+
+        }
+        return (actual.rawValue, speed)
     }
     
 }
+
+let carro = Auto()
+
+for i in 1...20{
+    let cambio = carro.cambioDeVelocidad()
+    print("\(i) \(cambio.actual), \(cambio.velocidadEnCadena)")
+}
+
